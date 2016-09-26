5884b9e4a2ed9f472ff02ef403f2ad48fd962a34
Quanlong <kyan.ql.he@gmail.com>
2016-09-26 14:50:33 +0800

    Track dist/

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+const { parseRawCommit } = require('conventional-changelog/lib/git');
+
+module.exports = function (pluginConfig, { commits }, cb) {
+  let type = null;
+
+  commits.map(commit => parseRawCommit(`${ commit.hash }\n${ commit.message }`)).filter(commit => !!commit).every(commit => {
+    if (commit.breaks.length) {
+      type = 'major';
+      return false;
+    }
+
+    if (commit.type === 'feat') type = 'minor';
+
+    if (!type && commit.type === 'fix') type = 'patch';
+
+    return true;
+  });
+
+  if (!type) type = 'minor';
+
+  cb(null, type);
+};
